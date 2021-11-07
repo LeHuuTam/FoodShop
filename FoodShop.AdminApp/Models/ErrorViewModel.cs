570d0672e442aac11dc95f5be2640ee0995f1fe0
Hữu Tâm Lê <lehuutam741@gmail.com>
2021-11-08 00:09:01 +0700

    Create admin app

---
+++ FoodShop.AdminApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FoodShop.AdminApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
