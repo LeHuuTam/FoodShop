e95cdc57cf9ee70bbfdd2d87b6d2bb81d93a0738
Hữu Tâm Lê <lehuutam741@gmail.com>
2021-11-08 01:17:32 +0700

    Create admin page

---
+++ FoodShop.AdminApp/Services/IUserApiClient.cs
@@ -0,0 +1,13 @@
+﻿using FoodShop.ViewModels.System.Users;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FoodShop.AdminApp.Services
+{
+    public interface IUserApiClient
+    {
+        Task<string> Authenticate(LoginRequest request);
+    }
+}
